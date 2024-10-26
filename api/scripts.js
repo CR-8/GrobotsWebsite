557e7eccae154a9b492cd239e346559cfc4c2ac3
Praharsh7270 <praharsh_bc22070@srmcem.ac.in>
2024-10-27 01:30:10 +0530

    db connected

--- api/scripts.js
+++ api/scripts.js
@@ -1,4 +1,18 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+        .then(()=>{
+            console.log("Databse is connected");
+        })
+        .catch((err)=>{
+            console.log("Database error detected");
+        })
 
 const app = express();
 const port = 3000;
