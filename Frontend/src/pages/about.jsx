9525347187b3a52f8926c0a1f762390a0654df0f
Pratyush Tiwari <pratyush_be23ds043@srmcem.ac.in>
2024-11-02 09:41:57 +0530

    Update about.jsx
    Added details for about.jsx , Asset will be given via whatsapp .

--- Frontend/src/pages/about.jsx
+++ Frontend/src/pages/about.jsx
@@ -3,6 +3,41 @@ import React from 'react'
 const about = () => {
   return (
     <div>About</div>
+      <h1>
+        <u> About Us </u>
+      </h1>
+      <h3>
+        Grobots: Building Innovators, Leaders, and Creators in Robotics at
+        SRMCEM
+      </h3>
+      {/* Description of the Grobots club, its purpose, and activities */}
+      <p>
+        Grobots is SRMCEM's premier technical club, dedicated to fostering
+        robotics excellence and technical prowess among students. As the driving
+        force behind the college's robotics events, Grobots is organized into
+        teams of Leads, Coordinators, and Assistant Coordinators to ensure
+        smooth, impactful operations. Beyond technical skills, Grobots instills
+        values like teamwork, professional ethics, and creativity, offering
+        students a platform to share and bring innovative ideas to life.
+        <br />
+        <br />
+        Each year, Grobots introduces freshers to the basics of robotics through
+        foundational classes, followed by thrilling events during Gantavya, the
+        college's tech fest, where students showcase their practical skills. Our
+        commitment extends to workshops and competitions, enabling students to
+        develop essential skills such as cooperation, sincerity, and
+        adaptability in professional environments.
+        <br />
+        <br />
+        From seminars and project development to intercollegiate events, Grobots
+        is a hub where students excel in robotics . Join us and experience a
+        journey that's as educational as it is transformative!
+      </p>
+      {/* Team image for visual representation */}
+      <img
+        src="WhatsApp Image 2024-10-30 at 22.42.27_14e0d7aa.jpg"
+        id="team_image"
+      />
   )
 }
 
